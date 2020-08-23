5577ef95b7a95d19423defd8dd1234bbe70a6f16
Андрей Лахов <lahovand@gmail.com>
2020-08-23 15:51:37 +0300

    Little improvements

---
+++ Classes/R6SPersonProfileStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AngleSharp;
+
+namespace WPF_HackersList.Classes
+{
+    public class R6SPersonProfileStatistics
+    {
+        public string adress = "https://r6.tracker.network/profile/pc/Ongin_rasp";
+
+        public R6SPersonProfileStatistics()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = context.OpenAsync(adress);
+        }
+    }
+}
